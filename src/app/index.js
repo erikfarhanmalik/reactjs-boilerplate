ece4456a942cf83df69a34c2161b6d573ec07e73
Erik Farhan Malik <Erik_M2267@mitrais.com>
2018-10-01 14:10:50 +0700

    created todo component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
-alert("It's works");
+var React = require('react');
+var createReactClass = require('create-react-class');
+var ReactDom = require('react-dom');
+
+var TodoComponent = createReactClass({
+  render: function(){
+    return(
+      <h1>Nice!</h1>
+    )
+  }
+});
+
+ReactDom.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
