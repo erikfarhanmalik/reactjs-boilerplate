c7e6978941c7f451e32bd87ad9745379af140ddf
Erik Farhan Malik <Erik_M2267@mitrais.com>
2018-10-01 15:01:31 +0700

    added base todo app code

--- src/app/index.js
+++ src/app/index.js
@@ -3,11 +3,45 @@ var createReactClass = require('create-react-class');
 var ReactDom = require('react-dom');
 
 var TodoComponent = createReactClass({
+  getInitialState: function(){
+    return {
+      todos: ['sleep', 'read', 'watch']
+    };
+  },
   render: function(){
+
+    setTimeout(function(){
+      this.setState({
+        // use this.setState({the state}) to modify the state -> will modify virtual and update the Real Dom efficiently by comparing both of DOM
+        todos: ['sleeping', 'reading', 'watching']
+      })
+      // bind external "this" into setTimeout context
+    }.bind(this), 5000);
+
+    var todos = this.state.todos;
+    todos = todos.map(function(item, index){
+      return (
+        <li>{item}</li>
+      )
+    })
+
     return(
-      <h1>Nice!</h1>
+      <div id="todo-list">
+        <p>{this.props.label}</p>
+        <p>{this.props.user.name}</p>
+        <p>{this.props.user.status}</p>
+
+        <br/>
+
+        <ul>{todos}</ul>
+      </div>
     )
   }
 });
 
-ReactDom.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
+var user  = {
+  name: 'erik',
+  status: 'online',
+}
+
+ReactDom.render(<TodoComponent label="Todo List" user={user}/>, document.getElementById('todo-wrapper'));
