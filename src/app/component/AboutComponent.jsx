c7af25b8a76f2e9bfd6a864273f113fe3952d6a3
ERIK FARHAN MALIK <erikfarhanmalik@gmail.com>
2018-10-02 12:42:23 +0700

    Added routing to the App

---
+++ src/app/component/AboutComponent.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var createReactClass = require('create-react-class');
+
+var AboutComponent = createReactClass({
+
+  render: function() {
+    return (<h2>All about me</h2>);
+  }
+
+});
+
+module.exports = AboutComponent;
