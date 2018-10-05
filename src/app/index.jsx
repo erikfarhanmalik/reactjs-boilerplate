45aac528db6b5552d54cc09d2d217386e97bd5f0
ERIK FARHAN MALIK <erikfarhanmalik@gmail.com>
2018-10-05 09:05:03 +0700

    Separated reducers into a specific file

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -6,38 +6,11 @@ import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 
+import {reducers} from './reducer/reducers';
 import TodoComponent from './component/TodoComponent';
 import AboutComponent from './component/AboutComponent';
 
-const reducer = function(state = {}, action) {
-
-  console.log('action');
-  console.log(action);
-  switch (action.type) {
-    case 'fetchTodo':
-      return action.payload;
-
-    case 'addTodo':
-      var todos = [...state.todos];
-      todos.push(action.payload);
-      return {
-        ...state,
-        todos: todos
-      };
-
-    case 'deleteTodo':
-      var todos = [...state.todos].filter((item, index) => item._links.self !== action.payload._links.self);
-      return {
-        ...state,
-        todos: todos
-      };
-
-    default:
-      return {todos: []};
-  }
-}
-
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 class App extends React.Component {
 
